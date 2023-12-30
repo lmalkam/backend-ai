ff025ba900aa57fb447db2e745669593d534ec2e
bprathamesh20 <bprathamesh2003@gmail.com>
2023-12-30 14:43:39 +0530

    added express app

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY, // Replace with your actual API key
 });
 
-async function main() {
+async function fetchData(prompt) {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "what is react js " }],
+    messages: [{ role: "system", content: `For the below given syllabus give me a school like test paper for exam .
+    -it should contain 10 questions
+    - all the questions should be of descriptive type
+    - questions should have variation in marks 
+    
+    ${prompt}` }],
     model: "gpt-3.5-turbo",
   });
 
   console.log(completion.choices[0].message.content);
 }
 
-main();
+export default fetchData;
+
+fetchData()
---
+++ server.js
@@ -0,0 +1,19 @@
+// index.js
+import express from 'express'
+import fetchData from '.';
+
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+
+
