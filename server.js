d1e3cea6dee12b62fed53c845789acd5f88f5b93
bprathamesh20 <bprathamesh2003@gmail.com>
2024-01-02 20:28:36 +0530

    added fetch history

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.post('/test', async (req, res) => {
   }
 });
 
+
 app.post('/notes', async (req, res) => {
 
   const query = req.body.syllabus;
@@ -70,6 +71,19 @@ app.post('/notes', async (req, res) => {
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json("Internal Server Error");
+  }
+});
+
+app.get('/history', async (req, res) => {
+  try {
+    // Fetch posts from the database for the specified user_id
+    const posts = await Post.find({});
+
+    // Send the retrieved posts as a response
+    res.json({ posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
